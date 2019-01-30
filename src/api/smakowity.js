7900ad7eaf214a09033bdc11e5e7b63293867260
Paweł Dymek <pawel.dymek@gmail.com>
2019-01-30 18:44:35 +0100

    Refactor API methods

--- src/api/smakowity.js
+++ src/api/smakowity.js
@@ -1,28 +1,18 @@
 const API_ENDPOINT = 'https://api.smakowity.pl/graphql';
 
 export function getRecommendedRecipes() {
-  return fetch(API_ENDPOINT, {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-          query: `
-            {
-                recipes(limit: 5) {
-                    id
-                    name
-                    img_url
-                }
+  return doRequest(
+      `
+        {
+            recipes(limit: 5) {
+                id
+                name
+                img_url
             }
-          `
-      })
-        .then(recipes => recipes.json())
-        .then(recipes => {
-            console.log(recipes);
-          return recipes;
-      });
-  });
+        }
+      `
+  )
+    .then(recipes => recipes.json());
 }
 
 function doRequest(query) {
